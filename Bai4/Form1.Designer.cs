8c5954c15863a9cf2abafd1c15bf7fa906b7bb3e
MinDuck <ducvominh@suctremmt.com>
2025-12-07 17:14:58 +0700

    Up Bai4

--- Bai4/Form1.Designer.cs
+++ Bai4/Form1.Designer.cs
@@ -2,39 +2,250 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
+            if (disposing && (components != null)) components.Dispose();
             base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.pbPoster = new System.Windows.Forms.PictureBox();
+            this.cboPhim = new System.Windows.Forms.ComboBox();
+            this.lblChonPhim = new System.Windows.Forms.Label();
+            this.panelRight = new System.Windows.Forms.Panel();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnBook = new System.Windows.Forms.Button();
+            this.grpCustomer = new System.Windows.Forms.GroupBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.grpSeats = new System.Windows.Forms.GroupBox();
+            this.flpGhe = new System.Windows.Forms.FlowLayoutPanel();
+            this.panelLeft.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPoster)).BeginInit();
+            this.panelRight.SuspendLayout();
+            this.grpCustomer.SuspendLayout();
+            this.grpSeats.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(1499, 50);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "RẠP CHIẾU PHIM - BOOKING ONLINE";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.BackColor = System.Drawing.Color.AliceBlue;
+            this.panelLeft.Controls.Add(this.pbPoster);
+            this.panelLeft.Controls.Add(this.cboPhim);
+            this.panelLeft.Controls.Add(this.lblChonPhim);
+            this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeft.Location = new System.Drawing.Point(0, 50);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(481, 653);
+            this.panelLeft.TabIndex = 1;
+            // 
+            // pbPoster
+            // 
+            this.pbPoster.BackColor = System.Drawing.Color.Gainsboro;
+            this.pbPoster.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbPoster.Location = new System.Drawing.Point(16, 80);
+            this.pbPoster.Name = "pbPoster";
+            this.pbPoster.Size = new System.Drawing.Size(423, 541);
+            this.pbPoster.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPoster.TabIndex = 2;
+            this.pbPoster.TabStop = false;
+            // 
+            // cboPhim
+            // 
+            this.cboPhim.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboPhim.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cboPhim.FormattingEnabled = true;
+            this.cboPhim.Location = new System.Drawing.Point(16, 40);
+            this.cboPhim.Name = "cboPhim";
+            this.cboPhim.Size = new System.Drawing.Size(423, 31);
+            this.cboPhim.TabIndex = 1;
+            this.cboPhim.SelectedIndexChanged += new System.EventHandler(this.CboPhim_SelectedIndexChanged);
+            // 
+            // lblChonPhim
+            // 
+            this.lblChonPhim.AutoSize = true;
+            this.lblChonPhim.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblChonPhim.Location = new System.Drawing.Point(12, 15);
+            this.lblChonPhim.Name = "lblChonPhim";
+            this.lblChonPhim.Size = new System.Drawing.Size(173, 25);
+            this.lblChonPhim.TabIndex = 0;
+            this.lblChonPhim.Text = "Chọn Phim (Web):";
+            // 
+            // panelRight
+            // 
+            this.panelRight.BackColor = System.Drawing.Color.AliceBlue;
+            this.panelRight.Controls.Add(this.lblStatus);
+            this.panelRight.Controls.Add(this.btnBook);
+            this.panelRight.Controls.Add(this.grpCustomer);
+            this.panelRight.Controls.Add(this.grpSeats);
+            this.panelRight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelRight.Location = new System.Drawing.Point(481, 50);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Size = new System.Drawing.Size(1018, 653);
+            this.panelRight.TabIndex = 2;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic);
+            this.lblStatus.ForeColor = System.Drawing.Color.DimGray;
+            this.lblStatus.Location = new System.Drawing.Point(474, 579);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(124, 20);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Vui lòng chọn vé...";
+            // 
+            // btnBook
+            // 
+            this.btnBook.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.btnBook.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBook.FlatAppearance.BorderSize = 0;
+            this.btnBook.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBook.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btnBook.ForeColor = System.Drawing.Color.White;
+            this.btnBook.Location = new System.Drawing.Point(23, 561);
+            this.btnBook.Name = "btnBook";
+            this.btnBook.Size = new System.Drawing.Size(372, 50);
+            this.btnBook.TabIndex = 2;
+            this.btnBook.Text = "ĐẶT VÉ & GỬI MAIL";
+            this.btnBook.UseVisualStyleBackColor = false;
+            this.btnBook.Click += new System.EventHandler(this.BtnBook_Click);
+            // 
+            // grpCustomer
+            // 
+            this.grpCustomer.Controls.Add(this.lblEmail);
+            this.grpCustomer.Controls.Add(this.txtEmail);
+            this.grpCustomer.Controls.Add(this.lblName);
+            this.grpCustomer.Controls.Add(this.txtName);
+            this.grpCustomer.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.grpCustomer.Location = new System.Drawing.Point(23, 382);
+            this.grpCustomer.Name = "grpCustomer";
+            this.grpCustomer.Size = new System.Drawing.Size(897, 146);
+            this.grpCustomer.TabIndex = 1;
+            this.grpCustomer.TabStop = false;
+            this.grpCustomer.Text = "Thông tin khách hàng";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblEmail.Location = new System.Drawing.Point(20, 70);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(55, 23);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtEmail.Location = new System.Drawing.Point(114, 67);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(400, 30);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblName.Location = new System.Drawing.Point(20, 35);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(88, 23);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Họ và tên:";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtName.Location = new System.Drawing.Point(114, 32);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(400, 30);
+            this.txtName.TabIndex = 1;
+            // 
+            // grpSeats
+            // 
+            this.grpSeats.Controls.Add(this.flpGhe);
+            this.grpSeats.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.grpSeats.Location = new System.Drawing.Point(20, 15);
+            this.grpSeats.Name = "grpSeats";
+            this.grpSeats.Size = new System.Drawing.Size(897, 344);
+            this.grpSeats.TabIndex = 0;
+            this.grpSeats.TabStop = false;
+            this.grpSeats.Text = "Màn Hình (Chọn ghế bên dưới)";
+            // 
+            // flpGhe
+            // 
+            this.flpGhe.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.flpGhe.AutoScroll = true;
+            this.flpGhe.Cursor = System.Windows.Forms.Cursors.Default;
+            this.flpGhe.Location = new System.Drawing.Point(209, 21);
+            this.flpGhe.Name = "flpGhe";
+            this.flpGhe.Padding = new System.Windows.Forms.Padding(10);
+            this.flpGhe.Size = new System.Drawing.Size(669, 317);
+            this.flpGhe.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1499, 703);
+            this.Controls.Add(this.panelRight);
+            this.Controls.Add(this.panelLeft);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quản lý Phòng vé (Lab 5) - Ông Trùm Winforms";
+            this.panelLeft.ResumeLayout(false);
+            this.panelLeft.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPoster)).EndInit();
+            this.panelRight.ResumeLayout(false);
+            this.panelRight.PerformLayout();
+            this.grpCustomer.ResumeLayout(false);
+            this.grpCustomer.PerformLayout();
+            this.grpSeats.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel panelLeft;
+        private System.Windows.Forms.PictureBox pbPoster;
+        private System.Windows.Forms.ComboBox cboPhim;
+        private System.Windows.Forms.Label lblChonPhim;
+        private System.Windows.Forms.Panel panelRight;
+        private System.Windows.Forms.GroupBox grpCustomer;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.GroupBox grpSeats;
+        private System.Windows.Forms.FlowLayoutPanel flpGhe;
+        private System.Windows.Forms.Button btnBook;
+        private System.Windows.Forms.Label lblStatus;
     }
 }
-
--- Bai4/Form1.cs
+++ Bai4/Form1.cs
@@ -1,20 +1,258 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Drawing; 
+using System.IO;      
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;     
+using System.Net.Http;
+using System.Net.Mail;
+using System.Net.Mime;
 using System.Windows.Forms;
+using HtmlAgilityPack; 
 
 namespace Bai4
 {
     public partial class Form1 : Form
     {
+        const string MY_EMAIL = "tdruucc20056@gmail.com";    
+        const string MY_APP_PASSWORD = "xrep oxeb tdqf aody";  
+
+        public class Movie
+        {
+            public string Title { get; set; }
+            public string PosterUrl { get; set; }
+            public decimal Price { get; set; } = 85000;
+        }
+
+        List<Movie> movies = new List<Movie>();
+        List<CheckBox> seatCheckBoxes = new List<CheckBox>();
+        HttpClient client = new HttpClient();
+
         public Form1()
         {
             InitializeComponent();
+            InitSeats();
+            LoadMovieDataFromWeb();
+        }
+
+        private void InitSeats()
+        {
+            flpGhe.Controls.Clear();
+            seatCheckBoxes.Clear();
+
+            char[] rows = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' };
+
+            foreach (char row in rows)
+            {
+                
+                for (int i = 1; i <= 10; i++)
+                {
+                    CheckBox chk = new CheckBox();
+                    chk.Text = $"{row}{i}"; 
+                    chk.Size = new Size(40, 32); 
+                    chk.Appearance = Appearance.Button;
+                    chk.TextAlign = ContentAlignment.MiddleCenter;
+                    chk.BackColor = Color.White;
+                    chk.Margin = new Padding(3);
+                    chk.Cursor = Cursors.Hand;
+                    chk.Font = new Font("Segoe UI", 8F, FontStyle.Bold); 
+
+                    chk.CheckedChanged += (s, e) => {
+                        var c = s as CheckBox;
+                        c.BackColor = c.Checked ? Color.Orange : Color.White;
+                        c.ForeColor = c.Checked ? Color.White : Color.Black;
+                    };
+
+                    flpGhe.Controls.Add(chk);
+                    seatCheckBoxes.Add(chk);
+
+                    if (i == 10)
+                    {
+                        flpGhe.SetFlowBreak(chk, true);
+                    }
+                }
+            }
+        }
+
+        private async void LoadMovieDataFromWeb()
+        {
+            btnBook.Enabled = false;
+            lblStatus.Text = "Đang kết nối Beta Cinemas...";
+
+            movies.Clear();
+            cboPhim.DataSource = null;
+
+            try
+            {
+                string url = "https://betacinemas.vn/phim.htm";
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36");
+
+                string htmlContent = await client.GetStringAsync(url);
+
+                HtmlAgilityPack.HtmlDocument document = new HtmlAgilityPack.HtmlDocument();
+                document.LoadHtml(htmlContent);
+                var movieNodes = document.DocumentNode.SelectNodes("//div[contains(@class, 'tab-pane') and contains(@class, 'active')]//div[contains(@class, 'col-lg-4')]");
+
+                
+                if (movieNodes != null)
+                {
+                    foreach (var node in movieNodes)
+                    {
+                        var titleNode = node.SelectSingleNode(".//h3/a");
+                        var imgNode = node.SelectSingleNode(".//img");
+
+                        if (titleNode != null && imgNode != null)
+                        {
+                            string title = titleNode.InnerText.Trim();
+                            string imgUrl = imgNode.GetAttributeValue("src", "");
+                            if (!imgUrl.StartsWith("http"))
+                            {
+                                imgUrl = "https://betacinemas.vn" + imgUrl;
+                            }
+
+                            if (!string.IsNullOrEmpty(title))
+                            {
+                                movies.Add(new Movie
+                                {
+                                    Title = title,
+                                    PosterUrl = imgUrl,
+                                    Price = 40000 
+                                });
+                            }
+                        }
+                    }
+                }
+
+                if (movies.Count > 0)
+                {
+                    cboPhim.DataSource = movies;
+                    cboPhim.DisplayMember = "Title";
+                    CboPhim_SelectedIndexChanged(null, null);
+
+                    lblStatus.Text = $"Đã tải thành công {movies.Count} phim.";
+                }
+                else
+                {
+                    lblStatus.Text = "Không tìm thấy phim nào (Web thay đổi cấu trúc?)";
+                    movies.Add(new Movie { Title = "Phim Mẫu (Lỗi Web)", PosterUrl = "https://via.placeholder.com/260x380" });
+                    cboPhim.DataSource = movies;
+                    cboPhim.DisplayMember = "Title";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi cào dữ liệu: " + ex.Message);
+                lblStatus.Text = "Lỗi kết nối.";
+            }
+            finally
+            {
+                btnBook.Enabled = true;
+            }
+        }
+        private void CboPhim_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var movie = cboPhim.SelectedItem as Movie;
+            if (movie == null) return;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(movie.PosterUrl))
+                    pbPoster.Load(movie.PosterUrl);
+            }
+            catch
+            {
+                pbPoster.Image = null;
+            }
+        }
+
+        private async void BtnBook_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text) || string.IsNullOrWhiteSpace(txtEmail.Text))
+            {
+                MessageBox.Show("Vui lòng nhập Tên và Email!", "Thiếu thông tin");
+                return;
+            }
+
+            var selectedSeats = seatCheckBoxes.Where(x => x.Checked).Select(x => x.Text).ToList();
+            if (selectedSeats.Count == 0)
+            {
+                MessageBox.Show("Vui lòng chọn ít nhất 1 ghế!", "Chưa chọn ghế");
+                return;
+            }
+
+            btnBook.Enabled = false;
+            btnBook.Text = "ĐANG GỬI...";
+            lblStatus.Text = "Đang kết nối SMTP...";
+
+            try
+            {
+                var movie = cboPhim.SelectedItem as Movie;
+                decimal total = selectedSeats.Count * movie.Price;
+                string seatStr = string.Join(", ", selectedSeats);
+
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(MY_EMAIL);
+                mail.To.Add(txtEmail.Text.Trim());
+                mail.Subject = $"Vé xem phim: {movie.Title}";
+
+                string htmlBody = $@"
+                    <div style='font-family: Arial; padding: 20px; border: 2px solid #3498db;'>
+                        <h2 style='color: #e74c3c;'>XÁC NHẬN ĐẶT VÉ THÀNH CÔNG</h2>
+                        <p>Xin chào <b>{txtName.Text}</b>,</p>
+                        <table style='width:100%'>
+                            <tr>
+                                <td style='vertical-align:top'>
+                                    <p>Phim: <b>{movie.Title}</b></p>
+                                    <p>Ghế: <b style='color:blue; font-size:18px'>{seatStr}</b></p>
+                                    <p>Giá vé: <b>{total:N0} VNĐ</b></p>
+                                    <p><i>Cảm ơn bạn đã sử dụng dịch vụ!</i></p>
+                                </td>
+                                <td>
+                                    <img src='cid:PosterImg' width='120' style='border:1px solid #ccc'/>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>";
+
+                AlternateView htmlView = AlternateView.CreateAlternateViewFromString(htmlBody, null, MediaTypeNames.Text.Html);
+
+                MemoryStream ms = new MemoryStream();
+                if (pbPoster.Image != null)
+                {
+                    pbPoster.Image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                    ms.Position = 0;
+                }
+                else
+                {
+                    new Bitmap(1, 1).Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                    ms.Position = 0;
+                }
+
+                LinkedResource imgRes = new LinkedResource(ms, "image/png");
+                imgRes.ContentId = "PosterImg";
+                htmlView.LinkedResources.Add(imgRes);
+
+                mail.AlternateViews.Add(htmlView);
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.EnableSsl = true;
+                smtp.Credentials = new NetworkCredential(MY_EMAIL, MY_APP_PASSWORD);
+
+                await smtp.SendMailAsync(mail);
+
+                MessageBox.Show("Đặt vé thành công! Vui lòng kiểm tra Email.", "Ông Trùm Winforms");
+                lblStatus.Text = "Gửi thành công.";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi gửi mail: " + ex.Message, "Error");
+                lblStatus.Text = "Lỗi.";
+            }
+            finally
+            {
+                btnBook.Enabled = true;
+                btnBook.Text = "ĐẶT VÉ & GỬI MAIL";
+            }
         }
     }
 }
